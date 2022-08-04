9034c20af676a26a625c076174f0b9c62bf9b28b
Acquati <leandroacquati@gmail.com>
2022-08-03 21:01:31 -0300

    add game menu

---
+++ assets/Scripts/MenuSceneManager.ts
@@ -0,0 +1,23 @@
+import { _decorator, Component, Node, Button, director } from 'cc'
+
+const { ccclass, property } = _decorator
+
+@ccclass('MenuSceneManager')
+export class MenuSceneManager extends Component {
+  @property({ type: Node })
+  private newGameButton: Node | null = null
+
+  @property({ type: Node })
+  private loadingLabel: Node | null = null
+
+  onLoad() {
+    this.newGameButton.on(Button.EventType.CLICK, this._newGame, this)
+  }
+
+  private _newGame() {
+    this.newGameButton.getComponent(Button).interactable = false
+    this.loadingLabel.active = true
+    console.log('Main.')
+    // director.loadScene('Main')
+  }
+}
