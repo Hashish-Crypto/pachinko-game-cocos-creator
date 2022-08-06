5af7c55f62723d0ce633472836f31cb6fece1c92
Acquati <leandroacquati@gmail.com>
2022-08-06 16:48:33 -0300

    add ball release system

--- assets/Scripts/MainSceneManager.ts
+++ assets/Scripts/MainSceneManager.ts
@@ -44,6 +44,7 @@ export class MainSceneManager extends Component {
   private _ball: Node | null = null
   private _cash: number = 10
   private _respawnBallEnabled = true
+  private _ballReleased = false
 
   onLoad() {
     // const canvasHeight = this.canvasNode.getComponent(UITransform).contentSize.height
@@ -55,51 +56,71 @@ export class MainSceneManager extends Component {
       for (let x = 0; x < this._pinGridWidth; x += 1) {
         this._pinGrid[y][x] = instantiate(this.pinPrefab)
         if (y % 2 === 0) {
-          this._pinGrid[y][x].setPosition(x * 60 + 30, -(y * 60))
-          this.pinRef.addChild(this._pinGrid[y][x])
-        } else {
-          this._pinGrid[y][x].setPosition(x * 60 + 60, -(y * 60))
+          this._pinGrid[y][x].setPosition(x * 58 + 75, -(y * 60))
           if (x !== this._pinGridWidth - 1) this.pinRef.addChild(this._pinGrid[y][x])
+        } else {
+          this._pinGrid[y][x].setPosition(x * 58 + 45, -(y * 60))
+          this.pinRef.addChild(this._pinGrid[y][x])
         }
       }
     }
 
     this._ball = instantiate(this.ballPrefab)
     this._ball.setPosition(this._randomBallPosition(), 0)
+    this._ball.getComponent(RigidBody2D).gravityScale = 0
+    if (randomRangeInt(0, 2) === 0) {
+      this._ball.getComponent(RigidBody2D).linearVelocity = new Vec2(6, 0)
+    } else {
+      this._ball.getComponent(RigidBody2D).linearVelocity = new Vec2(-6, 0)
+    }
     this.ballRef.addChild(this._ball)
 
     this._setCash(this._cash)
 
     PhysicsSystem2D.instance.on(Contact2DType.BEGIN_CONTACT, this._onBeginContact, this)
+    this.canvasNode.on(Node.EventType.TOUCH_START, this._onTouchScreen, this)
   }
 
-  // update(deltaTime: number) {}
+  update(deltaTime: number) {
+    if (!this._ballReleased) {
+      if (this._ball.position.x >= 150) {
+        this._ball.getComponent(RigidBody2D).linearVelocity = new Vec2(-6, 0)
+      } else if (this._ball.position.x <= -150) {
+        this._ball.getComponent(RigidBody2D).linearVelocity = new Vec2(6, 0)
+      }
+    }
+  }
 
   private _onBeginContact(a: Collider2D, b: Collider2D) {
     if (a.tag === 3 && b.tag === 1 && this._respawnBallEnabled) {
       this._respawnBall()
     } else if (a.tag === 4 && b.tag === 1 && this._respawnBallEnabled) {
       this._respawnBall()
-      if (a.node.name === 'Jackpot1000') {
-        this._setCash(Math.floor(this._cash * 1000 * 100) / 100)
-      } else if (a.node.name === 'Jackpot130') {
-        this._setCash(Math.floor(this._cash * 130 * 100) / 100)
-      } else if (a.node.name === 'Jackpot26') {
-        this._setCash(Math.floor(this._cash * 26 * 100) / 100)
-      } else if (a.node.name === 'Jackpot9') {
-        this._setCash(Math.floor(this._cash * 9 * 100) / 100)
-      } else if (a.node.name === 'Jackpot4') {
-        this._setCash(Math.floor(this._cash * 4 * 100) / 100)
-      } else if (a.node.name === 'Jackpot2') {
-        this._setCash(Math.floor(this._cash * 2 * 100) / 100)
+      if (a.node.name === 'Jackpot18') {
+        this._setCash(Math.floor(this._cash * 1.8 * 100) / 100)
+      } else if (a.node.name === 'Jackpot16') {
+        this._setCash(Math.floor(this._cash * 1.6 * 100) / 100)
+      } else if (a.node.name === 'Jackpot14') {
+        this._setCash(Math.floor(this._cash * 1.4 * 100) / 100)
+      } else if (a.node.name === 'Jackpot12') {
+        this._setCash(Math.floor(this._cash * 1.2 * 100) / 100)
+      } else if (a.node.name === 'Jackpot1') {
+        this._setCash(Math.floor(this._cash * 1 * 100) / 100)
       } else if (a.node.name === 'Jackpot08') {
         this._setCash(Math.floor(this._cash * 0.8 * 100) / 100)
+      } else if (a.node.name === 'Jackpot06') {
+        this._setCash(Math.floor(this._cash * 0.6 * 100) / 100)
       } else if (a.node.name === 'Jackpot04') {
         this._setCash(Math.floor(this._cash * 0.4 * 100) / 100)
       } else if (a.node.name === 'Jackpot02') {
-        this._setCash(Math.floor(this._cash * 0.5 * 100) / 100)
+        this._setCash(Math.floor(this._cash * 0.2 * 100) / 100)
       }
     }
+  }
+
+  private _onTouchScreen() {
+    this._ballReleased = true
+    this._ball.getComponent(RigidBody2D).gravityScale = 1
   }
 
   private _randomBallPosition() {
@@ -113,11 +134,16 @@ export class MainSceneManager extends Component {
 
   private _respawnBall() {
     this._respawnBallEnabled = false
+    this._ballReleased = false
     director.once(Director.EVENT_AFTER_PHYSICS, () => {
-      const ballBody = this._ball.getComponent(RigidBody2D)
-      ballBody.linearVelocity = new Vec2(0, 0)
-      ballBody.angularVelocity = 0
       this._ball.setPosition(this._randomBallPosition(), 0)
+      this._ball.getComponent(RigidBody2D).gravityScale = 0
+      if (randomRangeInt(0, 2) === 0) {
+        this._ball.getComponent(RigidBody2D).linearVelocity = new Vec2(6, 0)
+      } else {
+        this._ball.getComponent(RigidBody2D).linearVelocity = new Vec2(-6, 0)
+      }
+      this._ball.getComponent(RigidBody2D).angularVelocity = 0
       this._respawnBallEnabled = true
     })
   }
