0338d97694f520ed8177b9f1cdfbb1047555eda9
Acquati <leandroacquati@gmail.com>
2022-08-13 13:47:16 -0300

    add bet toggles

--- assets/Scripts/MenuSceneManager.ts
+++ assets/Scripts/MenuSceneManager.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, Button, director, find, Label } from 'cc'
+import { _decorator, Component, Node, Button, director, find, Label, ToggleComponent } from 'cc'
 import { PersistentNode } from './PersistentNode'
 
 const { ccclass, property } = _decorator
@@ -14,6 +14,9 @@ export class MenuSceneManager extends Component {
   @property({ type: Node })
   private loadingLabel: Node | null = null
 
+  @property([ToggleComponent])
+  private betToggles: ToggleComponent[] = []
+
   private _persistentNode: PersistentNode | null = null
 
   onLoad() {
@@ -21,6 +24,29 @@ export class MenuSceneManager extends Component {
 
     this._setCash(this._persistentNode.cash)
 
+    this.betToggles.forEach((betToggle) => {
+      if (betToggle.isChecked) {
+        betToggle.interactable = false
+
+        if (betToggle.node.name === 'Toggle1') {
+          this._persistentNode.betTarget = 0.75
+          this._persistentNode.betMultiplier = 1.75
+        } else if (betToggle.node.name === 'Toggle2') {
+          this._persistentNode.betTarget = 0.5
+          this._persistentNode.betMultiplier = 1.5
+        } else if (betToggle.node.name === 'Toggle3') {
+          this._persistentNode.betTarget = 2
+          this._persistentNode.betMultiplier = 2
+        } else if (betToggle.node.name === 'Toggle4') {
+          this._persistentNode.betTarget = 3
+          this._persistentNode.betMultiplier = 3
+        }
+      }
+    })
+
+    this.betToggles.forEach((betToggle) => {
+      betToggle.node.on(ToggleComponent.EventType.CLICK, this._setBetTarget, this)
+    })
     this.newGameButton.on(Button.EventType.CLICK, this._newGame, this)
   }
 
@@ -33,5 +59,38 @@ export class MenuSceneManager extends Component {
   private _setCash(value: number) {
     this._persistentNode.cash = value
     this.cashLabel.string = 'HC$ ' + this._persistentNode.cash.toFixed(2)
+  }
+
+  private _setBetTarget(betToggle: ToggleComponent) {
+    if (betToggle.isChecked) return
+
+    if (betToggle.node.name === 'Toggle1') {
+      betToggle.interactable = false
+      this._persistentNode.betTarget = 0.75
+      this._persistentNode.betMultiplier = 1.75
+    } else if (betToggle.node.name === 'Toggle2') {
+      betToggle.interactable = false
+      this._persistentNode.betTarget = 0.5
+      this._persistentNode.betMultiplier = 1.5
+    } else if (betToggle.node.name === 'Toggle3') {
+      betToggle.interactable = false
+      this._persistentNode.betTarget = 2
+      this._persistentNode.betMultiplier = 2
+    } else if (betToggle.node.name === 'Toggle4') {
+      betToggle.interactable = false
+      this._persistentNode.betTarget = 3
+      this._persistentNode.betMultiplier = 3
+    }
+
+    const otherBetToggles = this.betToggles.filter((toggle) => toggle.node.name !== betToggle.node.name)
+    otherBetToggles.forEach((toggle) => {
+      toggle.isChecked = false
+      toggle.interactable = true
+    })
+
+    console.log(otherBetToggles)
+    console.log(betToggle.node.name)
+    console.log(this._persistentNode.betTarget)
+    console.log(this._persistentNode.betMultiplier)
   }
 }
--- assets/Scripts/PersistentNode.ts
+++ assets/Scripts/PersistentNode.ts
@@ -5,8 +5,11 @@ const { ccclass } = _decorator
 @ccclass('PersistentNode')
 export class PersistentNode extends Component {
   public cash: number = 100
+  public pot: number = 0
   public throwRoundsMax: number = 2
   public throwRounds: number | null = null
+  public betMultiplier: number | null = null
+  public betTarget: number | null = null
 
   onLoad() {
     game.addPersistRootNode(this.node)
